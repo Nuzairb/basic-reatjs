6408f77b88b798b94c7fda1e52f1fe3844a5efe4
Nuzair Ibrahim <nuzairb@gmail.com>
2018-11-19 13:56:14 +0500

    init

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,23 @@
-console.log("It works");
+var React = require("react");
+var ReactDOM = require("react-dom");
+require('index.css');
+
+// State
+// Lifecycle events
+// UI
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        Hellow World!
+      </div>
+    )
+  }
+}
+
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('app')
+)
--- webpack.config.js
+++ webpack.config.js
@@ -3,25 +3,17 @@ var path = require("path");
 var DIST_DIR = path.resolve(__dirname, "dist");
 var SRC_DIR = path.resolve(__dirname, "src");
 
-var config = {
+module.exports = {
   entry: SRC_DIR + "/app/index.js",
   output: {
-    path: DIST_DIR + "/app",
-    filename: "bundle.js",
-    publicPath: "/app/"
+    path: DIST_DIR +  "/app",
+    filename: "index_bundle.js"
   },
   module: {
     rules: [
-      {
-        test: /\.js$/,
-        include: SRC_DIR,
-        loader: "babel-loader",
-        qurey: {
-          presets: ["react", "es2015", "stage-2"]
-        }
-      }
+      {test: /\.(js)$/, use: "babel-loader" },
+      {test: /\.css$/, use: ["style-loader", "css-loader"]}
     ]
-  }
-};
-
-module.exports = config;
+  },
+  mode: "development",
+}
