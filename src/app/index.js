2799d47d8d1aef4649f21070b682fa7b14bee7cf
Nuzair Ibrahim <nuzairb@gmail.com>
2018-11-19 15:23:04 +0500

    coded the first component with react

--- src/app/index.js
+++ src/app/index.js
@@ -9,8 +9,12 @@ require('./index.css');
 class App extends React.Component {
   render() {
     return (
-      <div>
-        Hello world!
+      <div className="container">
+        <div className="row">
+          <div className="col-xs-10 col-xs-offset-1">
+            <h1>Hello world!</h1>
+          </div>
+        </div>
       </div>
     )
   }
