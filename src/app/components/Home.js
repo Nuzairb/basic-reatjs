bd2e34fe26060059c7c8c53a9be15168918ffc64
Nuzair Ibrahim <nuzairb@gmail.com>
2018-11-20 10:57:20 +0500

    init

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -3,21 +3,11 @@ import PropTypes from "prop-types";
 
 export class Home extends React.Component {
   render() {
-    var text = "Something!"
     return (
       <div>
         <p>In a new component!</p>
-        <p>{text}</p>
         <p>Your name is {this.props.name}, your age is {this.props.age}</p>
-        <p>User Object => Name: {this.props.user.name}</p>
         <div>
-          <h4>hobbies</h4>
-          <ul>
-            {this.props.user.hobbies.map((hobby, i) => <li key={i}>{hobby}</li>)}
-          </ul>
-        </div>
-        <hr/>
-        {this.props.childern}
       </div>
     );
   }
@@ -25,7 +15,5 @@ export class Home extends React.Component {
 
 Home.propTypes = {
   name: PropTypes.string,
-  age: PropTypes.number,
-  user: PropTypes.object,
-  childern: PropTypes.element.isRequired
+  age: PropTypes.number
 };
