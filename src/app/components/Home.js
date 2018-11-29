99ac31b06abb50bd40a8cb3f7aaf424292362701
Nuzair Ibrahim <nuzairb@gmail.com>
2018-11-29 18:15:41 +0500

    init

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -31,6 +31,9 @@ export class Home extends React.Component {
     
     shouldComponentUpdate(nextProps, nextState) {
         console.log("Should componnet update", nextProps, nextState);
+        if (nextState.status === 1) {
+            return false;
+        }
         return true;
     }
 
