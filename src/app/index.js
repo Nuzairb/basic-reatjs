8eb6dc3beb4a72bf6c0f186eb24747cb2e514a62
Nuzair Ibrahim <nuzairb@gmail.com>
2018-11-29 18:12:19 +0500

    init

--- src/app/index.js
+++ src/app/index.js
@@ -43,20 +43,21 @@ class App extends React.Component {
                 />
             );
         }
-        return (
-            <div className="container">
-                <div className="row">
-                    <div className="col-xs-10 col-xs-offset-1">
-                        <Header homeLink={this.state.homeLink}/>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-xs-10 col-xs-offset-1">
-                        {homeCmp}
-                    </div>
-                </div>
+        return <div className="container">
+            <div className="row">
+              <div className="col-xs-10 col-xs-offset-1">
+                <Header homeLink={this.state.homeLink} />
+              </div>
             </div>
-        );
+            <div className="row">
+              <div className="col-xs-10 col-xs-offset-1">{homeCmp}</div>
+            </div>
+            <div className="row">
+              <div className="col-xs-10 col-xs-offset-1">
+                <button onClick={this.onChangeHomeMounted.bind(this)} className="btn btn-primary">(un)Mount Home Component</button>
+              </div>
+            </div>
+          </div>;
     }
 }
 
