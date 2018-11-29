f54894807f9e9a515e0c353b1fbb8236f38ade99
Nuzair Ibrahim <nuzairb@gmail.com>
2018-11-29 20:58:05 +0500

    init

---
+++ src/app/components/User.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export class User extends React.Component {
+    render() {
+        return (
+            <div>
+                <h3>The Use Page</h3>
+                <p>User ID: </p>
+            </div>
+        )
+    }
+}
